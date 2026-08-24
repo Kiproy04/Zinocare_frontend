6f02ef06841f14990dc9b47889e3285f30452065
Kiproy04 <delleroy04@gmail.com>
2026-08-25 00:33:00 +0300

    Sentry error test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import { Toaster } from 'react-hot-toast'
-import * as Sentry from "@sentry/react";
+
 import './index.css'
 import App from './App.jsx'
-
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
@@ -16,20 +16,19 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   sendDefaultPii: false,
-});
 
-const container = document.getElementById("app") || document.getElementById("root");
-const root = createRoot(container);
+  environment: 'production',
+})
+
+const container =
+  document.getElementById('app') ||
+  document.getElementById('root')
+
+const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Toaster />
+  </StrictMode>
 )
-

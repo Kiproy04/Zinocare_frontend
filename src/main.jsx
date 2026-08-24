e09d15e058722f2e381851bf9153158755c0883b
Kiproy04 <delleroy04@gmail.com>
2026-08-24 23:52:46 +0300

    Sentry error test

--- src/main.jsx
+++ src/main.jsx
@@ -7,23 +7,17 @@ import App from './App.jsx'
 
 
 Sentry.init({
-  // Use your environment variable or directly paste your Sentry DSN string here
   dsn: import.meta.env.VITE_SENTRY_DSN,
 
   integrations: [
     Sentry.browserTracingIntegration(),
   ],
 
-  // Set to 1.0 while testing locally so 100% of events/traces are sent
   tracesSampleRate: 1.0,
 
-  dataCollection: {
-    userInfo: false,
-    httpBodies: []
-  }
+  sendDefaultPii: false,
 });
 
-// Ensure "app" matches the id in your index.html (Vite defaults to "root")
 const container = document.getElementById("app") || document.getElementById("root");
 const root = createRoot(container);
 
